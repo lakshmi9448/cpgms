dce0f1c493690089ae91bc6e18a88e552c8fc5a5
lakshmi9448 <61342770+lakshmi9448@users.noreply.github.com>
2020-03-10 10:18:07 +0530

    Add files via upload

---
+++ ly.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ void main()
+  { int a;
+    printf("enter a year to check if its a leap year");
+    scanf("%d",&a);
+    if(a%4==0)
+    {
+      printf("its a leap year");
+    }
+    else
+    printf("not a leap year");
+  }
+    
